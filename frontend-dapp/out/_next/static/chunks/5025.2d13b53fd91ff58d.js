bf044d55f2e3e2b1a7d62db6b5a1930eb5200b31
hendri2808 <rahmathst99@gmail.com>
2024-12-15 01:47:10 +0700

    Add out folder for deployment

---
+++ frontend-dapp/out/_next/static/chunks/5025.2d13b53fd91ff58d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5025],{25025:function(e){e.exports=JSON.parse('[{"type":"function","name":"supportsInterface","inputs":[{"name":"interfaceId","type":"bytes4","internalType":"bytes4"}],"outputs":[{"name":"","type":"bool","internalType":"bool"}],"stateMutability":"view"}]')}}]);
